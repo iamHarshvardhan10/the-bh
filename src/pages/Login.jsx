da6a04507fdec02721bc9dcaa2855bcc4f5dfc79
Harshvardhan <shreeharshpb11@gmail.com>
2025-06-17 01:18:31 +0530

    register and login page UI design

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="register-container">
+      <div className="register-form">
+        <form>
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="password" />
+          </div>
+          <div className="form-field">
+            <p>
+              Dont have account! Please <Link to={"/register"}>Register</Link>
+            </p>
+          </div>
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
-export default Login
+export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,9 +1,34 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Register = () => {
   return (
-    <div>Register</div>
-  )
-}
+    <div className="register-container">
+      <div className="register-form">
+        <form>
+          <div className="form-field">
+            <label htmlFor="userName">User Name</label>
+            <input type="text" id="userName" placeholder="User Name" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id="email" placeholder="Email" />
+          </div>
+          <div className="form-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" placeholder="password" />
+          </div>
+          <div className="form-field">
+            <p>
+              Already Have an Account! Please <Link to={"/login"}>login</Link>
+            </p>
+          </div>
+
+          <button className="btn">Submit</button>
+        </form>
+      </div>
+    </div>
+  );
+};
 
 export default Register;
